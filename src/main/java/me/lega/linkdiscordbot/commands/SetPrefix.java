6185873e8ae26b615daa098a018c20c7e20975b3
notlega <onghinjourn@gmail.com>
2022-08-11 23:23:54 +0800

    refactor: Error handling and code cleanup

--- src/main/java/me/lega/linkdiscordbot/commands/SetPrefix.java
+++ src/main/java/me/lega/linkdiscordbot/commands/SetPrefix.java
@@ -1,12 +1,6 @@
 package me.lega.linkdiscordbot.commands;
 
-import me.lega.linkdiscordbot.CommandContainer;
-import me.lega.linkdiscordbot.classes.DiscordServers;
-import me.lega.linkdiscordbot.classes.DiscordUsers;
-import me.lega.linkdiscordbot.classes.Prefixes;
-import me.lega.linkdiscordbot.database.GetPrefix;
-import me.lega.linkdiscordbot.database.InsertDiscordServer;
-import me.lega.linkdiscordbot.database.InsertPrefix;
+import me.lega.linkdiscordbot.classes.CommandContainer;
 import me.lega.linkdiscordbot.database.UpdatePrefix;
 
 public class SetPrefix {
@@ -15,25 +9,31 @@ public class SetPrefix {
 
     }
 
-    public void SetPrefix(DiscordUsers discordUsers, CommandContainer commandContainer) {
+    public void setPrefix(CommandContainer commandContainer) {
 
-        int numRowAffected;
-        InsertDiscordServer insertDiscordServer = new InsertDiscordServer();
-        Prefixes prefixes = null;
-        DiscordServers discordServers = insertDiscordServer.InsertDiscordServer(commandContainer.getEvent());
-        GetPrefix getPrefix = new GetPrefix();
-        InsertPrefix insertPrefix = new InsertPrefix();
         UpdatePrefix updatePrefix = new UpdatePrefix();
-        prefixes = getPrefix.GetPrefix(discordServers);
 
-        if (prefixes == null) {
-            numRowAffected = insertPrefix.InsertPrefix(discordServers, commandContainer.getContentOfCommand()[0]);
-        } else {
-            numRowAffected = updatePrefix.UpdatePrefix(discordServers, commandContainer);
+        if (commandContainer.getContentOfCommand() == null) {
+            commandContainer.getEvent().getMessage().reply("You need to specify a prefix!").queue();
+            return;
         }
 
+        if (commandContainer.getContentOfCommand().length() > 1) {
+            commandContainer.getEvent().getMessage().reply("Prefixes can only be one character long!").queue();
+            return;
+        }
+
+        if (commandContainer.getCurrentPrefix().getPrefix().equals(commandContainer.getContentOfCommand())) {
+            commandContainer.getEvent().getMessage().reply("That prefix is already set!").queue();
+            return;
+        }
+
+        int numRowAffected = updatePrefix.updatePrefix(commandContainer);
+
         if (numRowAffected > 0) {
-            commandContainer.getEvent().getMessage().reply("Prefix has been changed to <" + commandContainer.getContentOfCommand()[0] + "> !").queue();
+            commandContainer.getEvent().getMessage().reply("Prefix has been changed to <" + commandContainer.getContentOfCommand() + "> !").queue();
+        } else if (numRowAffected == 0) {
+            commandContainer.getEvent().getMessage().reply("Something went wrong!").queue();
         }
     }
 }
