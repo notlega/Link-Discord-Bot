3fc9d8ab1d124ef7d9074d41c49b34c431014853
notlega <onghinjourn@gmail.com>
2022-08-26 15:11:43 +0800

    feat: Removes discordServers and prefixes from structure

--- src/main/java/records/DiscordServer.java
+++
@@ -1,5 +0,0 @@
-package records;
-
-public record DiscordServer(int id, long discordServerID, String discordServerName) {
-
-}
--- src/main/java/records/Prefix.java
+++
@@ -1,5 +0,0 @@
-package records;
-
-public record Prefix(int id, String prefix, int discordServerID) {
-	public static final String DEFAULT_PREFIX = "!";
-}
