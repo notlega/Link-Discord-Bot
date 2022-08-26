3e21f21ddde78ecc0e03bcd9bcd14df6d58b9d4c
notlega <onghinjourn@gmail.com>
2022-08-26 15:14:11 +0800

    feat: Adds readyEventListener
    Adds it into JDA instance

--- src/main/java/LinkDiscordBot.java
+++ src/main/java/LinkDiscordBot.java
@@ -2,7 +2,6 @@ import io.github.cdimascio.dotenv.Dotenv;
 import listeners.SlashCommandInteractionEventListener;
 import util.CommandHandler;
 import listeners.GuildEventListener;
-import listeners.MessageEventListener;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.OnlineStatus;
 import net.dv8tion.jda.api.entities.Activity;
@@ -28,7 +27,6 @@ public class LinkDiscordBot {
 				.setStatus(OnlineStatus.IDLE)
 				.setActivity(Activity.playing("with waifus"))
 				.setRequestTimeoutRetry(true)
-				.addEventListeners(new MessageEventListener())
 				.addEventListeners(new SlashCommandInteractionEventListener())
 				.addEventListeners(new GuildEventListener())
 				.build();
