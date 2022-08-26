f8e0c79631cc2349775fb5b45e210141a58bed8b
notlega <onghinjourn@gmail.com>
2022-08-27 03:20:09 +0800

    feat: Updates caseConverter
    Renames variables for readability

--- src/main/java/util/CaseConverter.java
+++ src/main/java/util/CaseConverter.java
@@ -4,44 +4,44 @@ public class CaseConverter {
 
     /**
      * Converts a string to snakeCase.
-     * @param string The string to convert.
+     * @param camelCaseString The string to convert.
      * @return The string in snakeCase.
      */
-    public static String toSnakeCase(String string) {
-        StringBuilder newString = new StringBuilder(Capitalisation.decapitalise(string));
+    public static String toSnakeCase(String camelCaseString) {
+        StringBuilder snakeCaseString = new StringBuilder(Capitalisation.decapitalise(camelCaseString));
 
-        for (int i = 0; i < newString.length(); i++) {
-            if (Character.isUpperCase(newString.charAt(i))) {
-                newString.setCharAt(i, Character.toLowerCase(newString.charAt(i)));
-                newString.insert(i, "_");
+        for (int i = 0; i < snakeCaseString.length(); i++) {
+            if (Character.isUpperCase(snakeCaseString.charAt(i))) {
+                snakeCaseString.setCharAt(i, Character.toLowerCase(snakeCaseString.charAt(i)));
+                snakeCaseString.insert(i, "_");
                 i--;
             }
         }
-        return newString.toString();
+        return snakeCaseString.toString();
     }
 
     /**
      * Converts a string to camelCase.
-     * @param string The string to convert.
+     * @param snakeCaseString The string to convert.
      * @param capitaliseFirstLetter Whether to capitalise the first letter of the string.
      * @return The string in camelCase.
      */
-    public static String toCamelCase(String string, boolean capitaliseFirstLetter) {
-        StringBuilder newString;
+    public static String toCamelCase(String snakeCaseString, boolean capitaliseFirstLetter) {
+        StringBuilder camelCaseString;
 
         if (capitaliseFirstLetter) {
-            newString = new StringBuilder(Capitalisation.capitalise(string));
+            camelCaseString = new StringBuilder(Capitalisation.capitalise(snakeCaseString));
         } else {
-            newString = new StringBuilder(Capitalisation.decapitalise(string));
+            camelCaseString = new StringBuilder(Capitalisation.decapitalise(snakeCaseString));
         }
 
-        for (int i = 0; i < newString.length(); i++) {
-            if (newString.charAt(i) == '_') {
-                newString.setCharAt(i + 1, Character.toUpperCase(newString.charAt(i + 1)));
-                newString.deleteCharAt(i);
+        for (int i = 0; i < camelCaseString.length(); i++) {
+            if (camelCaseString.charAt(i) == '_') {
+                camelCaseString.setCharAt(i + 1, Character.toUpperCase(camelCaseString.charAt(i + 1)));
+                camelCaseString.deleteCharAt(i);
                 i--;
             }
         }
-        return newString.toString();
+        return camelCaseString.toString();
     }
 }
