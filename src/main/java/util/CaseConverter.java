8d1336d6db1f1ccf0ec9f1dc057984be50eae381
notlega <onghinjourn@gmail.com>
2022-08-27 03:18:06 +0800

    feat: Updates caseConverter
    Adds JavaDoc to both methods

--- src/main/java/util/CaseConverter.java
+++ src/main/java/util/CaseConverter.java
@@ -2,19 +2,46 @@ package util;
 
 public class CaseConverter {
 
+    /**
+     * Converts a string to snakeCase.
+     * @param string The string to convert.
+     * @return The string in snakeCase.
+     */
     public static String toSnakeCase(String string) {
-        StringBuilder newString = new StringBuilder(Capitalisation.capitalise(string));
+        StringBuilder newString = new StringBuilder(Capitalisation.decapitalise(string));
+
         for (int i = 0; i < newString.length(); i++) {
-            if (Character.isUpperCase(string.charAt(i))) {
+            if (Character.isUpperCase(newString.charAt(i))) {
                 newString.setCharAt(i, Character.toLowerCase(newString.charAt(i)));
                 newString.insert(i, "_");
                 i--;
             }
         }
-        return string;
+        return newString.toString();
     }
 
-    public static String toCamelCase(String string) {
-        return string.toUpperCase();
+    /**
+     * Converts a string to camelCase.
+     * @param string The string to convert.
+     * @param capitaliseFirstLetter Whether to capitalise the first letter of the string.
+     * @return The string in camelCase.
+     */
+    public static String toCamelCase(String string, boolean capitaliseFirstLetter) {
+        StringBuilder newString;
+
+        if (capitaliseFirstLetter) {
+            newString = new StringBuilder(Capitalisation.capitalise(string));
+        } else {
+            newString = new StringBuilder(Capitalisation.decapitalise(string));
+        }
+
+        for (int i = 0; i < newString.length(); i++) {
+            if (newString.charAt(i) == '_') {
+                newString.setCharAt(i + 1, Character.toUpperCase(newString.charAt(i + 1)));
+                newString.deleteCharAt(i);
+                i--;
+            }
+        }
+        return newString.toString();
     }
 }
