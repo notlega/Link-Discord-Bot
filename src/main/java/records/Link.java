876da4ae3ff9398b64b242f7543f56b40a989602
notlega <onghinjourn@gmail.com>
2022-08-31 23:08:23 +0800

    feat: Removes regex for link patterns

--- src/main/java/records/Link.java
+++ src/main/java/records/Link.java
@@ -1,8 +1,6 @@
 package records;
 
 import java.sql.Timestamp;
-import java.util.regex.Pattern;
 
 public record Link(int id, String link, String linkName, int discordUserID, Timestamp createdAt) {
-	public static final Pattern LINK_PATTERN = Pattern.compile("[(htps)?:/w.a-zA-Z0-9@%_+~#=]{2,256}\\.[a-z]{2,6}\\b([-a-zA-Z0-9@:%_+.~#?&/=]*)");
 }
