88b6946957ae84353eebbe881c092890d71f52c9
notlega <onghinjourn@gmail.com>
2022-09-01 21:38:32 +0800

    fix: Implements working twitterModels

--- src/main/java/models/TwitterModel.java
+++ src/main/java/models/TwitterModel.java
@@ -9,44 +9,35 @@ import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.utils.URIBuilder;
 import org.apache.http.impl.client.HttpClients;
-import twitter4j.Query;
-import twitter4j.Twitter;
-import twitter4j.TwitterFactory;
-import twitter4j.conf.ConfigurationBuilder;
+import org.apache.http.util.EntityUtils;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
 
 public class TwitterModel {
 
-    public static String getTweetImageLink() {
-        ConfigurationBuilder cb = new ConfigurationBuilder();
-        Dotenv dotenv = Dotenv.configure().load();
-        cb.setDebugEnabled(true)
-                .setOAuthConsumerKey(dotenv.get("TWITTER_CONSUMER_KEY"))
-                .setOAuthConsumerSecret(dotenv.get("TWITTER_CONSUMER_SECRET"))
-                .setOAuthAccessToken(dotenv.get("TWITTER_ACCESS_TOKEN"))
-                .setOAuthAccessTokenSecret(dotenv.get("TWITTER_ACCESS_TOKEN_SECRET"));
-        TwitterFactory twitterFactory = new TwitterFactory(cb.build());
-        Twitter twitter = twitterFactory.getInstance();
-        Query query = new Query("from:TwitterDev");
-        return "";
-    }
+    public static String getTweetImageLink(String link) throws URISyntaxException, IOException, ParseException {
+        String[] parsedLink = link.replaceAll("^(https?://)?(www.)?twitter.com/", "").split("/");
+        if (!parsedLink[1].equals("status")) {
+            return null;
+        }
 
-    public static Object getTweets(String[] idArray) throws URISyntaxException, IOException {
+        if (parsedLink[2].contains("?")) {
+            parsedLink[2] = parsedLink[2].split("\\?")[0];
+        }
+
         HttpClient httpClient = HttpClients.custom()
                 .setDefaultRequestConfig(RequestConfig.custom()
                         .setCookieSpec(CookieSpecs.STANDARD).build()).build();
 
         try {
             URIBuilder uriBuilder = new URIBuilder("https://api.twitter.com/2/tweets");
-            if (idArray.length > 1) {
-                uriBuilder.addParameter("ids", String.join(",", idArray));
-            } else {
-                uriBuilder.addParameter("ids", idArray[0]);
-            }
-            uriBuilder.addParameter("expansions", "attachments.media_keys,author_id");
-            uriBuilder.addParameter("tweet.fields", "id,text,created_at,media_keys,author_id");
+            uriBuilder.addParameter("ids", parsedLink[2]);
+            uriBuilder.addParameter("expansions", "attachments.media_keys");
             uriBuilder.addParameter("media.fields", "media_key,type,url,preview_image_url");
 
             HttpGet httpGet = new HttpGet(uriBuilder.build());
@@ -54,10 +45,15 @@ public class TwitterModel {
             httpGet.setHeader("Content-Type", "application/json");
 
             HttpResponse response = httpClient.execute(httpGet);
-            System.out.println(response.getStatusLine().getStatusCode());
-            System.out.println(response.getEntity().toString());
+            String responseEntityStr = EntityUtils.toString(response.getEntity(), "UTF-8");
             if (response.getStatusLine().getStatusCode() == 200) {
-                return response.getEntity().getContent();
+                JSONObject jsonObject = (JSONObject) new JSONParser().parse(responseEntityStr);
+                JSONObject includes = (JSONObject) jsonObject.get("includes");
+                if (includes == null) {
+                    return null;
+                }
+                JSONArray mediaArray = (JSONArray) includes.get("media");
+                return mediaArray.get(0).toString();
             }
         } catch (URISyntaxException e) {
             throw new URISyntaxException("Invalid URI", e.getInput());
@@ -65,9 +61,10 @@ public class TwitterModel {
             throw new ClientProtocolException("Invalid protocol");
         } catch (IOException e) {
             throw new IOException("Invalid IO");
+        } catch (ParseException e) {
+            throw new ParseException(0, e);
         }
 
-        return new Object() {
-        };
+        return null;
     }
 }
